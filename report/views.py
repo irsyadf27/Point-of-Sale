47f7c557b8f9f770f2226da0901826fbc3388bdf
irsyad fauzan <irsyad.fauzan777@gmail.com>
2018-03-16 14:57:09 +0700

    tambah filter tahun

--- report/views.py
+++ report/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect, HttpResponse
-from django.db.models.functions import TruncMonth, TruncDay, TruncDate
+from django.db.models.functions import TruncMonth, TruncYear, TruncDate
 from django.db.models import Sum
 from cashier.models import Invoice
 from datetime import datetime
@@ -19,7 +19,16 @@ def get_json(request):
     a = datetime.strptime(start_date, "%Y-%m-%d %H:%M:%S")
     b = datetime.strptime(end_date, "%Y-%m-%d %H:%M:%S")
     selisih = b - a
-    if selisih.days > 61:
+    if selisih.days > 365:
+        total_uang_masuk = Invoice.objects.filter(created_at__range=[start_date,end_date]).annotate(tahun=TruncMonth('created_at')).values('tahun').annotate(jml=Sum('total')).values('tahun', 'jml')
+        total_keuntungan = Invoice.objects.filter(created_at__range=[start_date,end_date]).annotate(tahun=TruncMonth('created_at')).values('tahun').annotate(jml=(Sum('total') - Sum('cost_total'))).values('jml')
+        total_item = Invoice.objects.filter(created_at__range=[start_date,end_date]).annotate(tahun=TruncMonth('created_at')).values('tahun').annotate(jml=Sum('qty')).values('jml')
+
+        hari = [i['tahun'].year for i in total_uang_masuk]
+        uang_masuk = [i['jml'] for i in total_uang_masuk]
+        keuntungan = [i['jml'] for i in total_keuntungan]
+        item = [i['jml'] for i in total_item]
+    elif selisih.days > 61:
         total_uang_masuk = Invoice.objects.filter(created_at__range=[start_date,end_date]).annotate(bulan=TruncMonth('created_at')).values('bulan').annotate(jml=Sum('total')).values('bulan', 'jml')
         total_keuntungan = Invoice.objects.filter(created_at__range=[start_date,end_date]).annotate(bulan=TruncMonth('created_at')).values('bulan').annotate(jml=(Sum('total') - Sum('cost_total'))).values('jml')
         total_item = Invoice.objects.filter(created_at__range=[start_date,end_date]).annotate(bulan=TruncMonth('created_at')).values('bulan').annotate(jml=Sum('qty')).values('jml')
